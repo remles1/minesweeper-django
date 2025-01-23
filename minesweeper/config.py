147bbf31ef68af2329c311f4d469cdfec5b8dbdb
remles <115708921+remles1@users.noreply.github.com>
2025-01-23 22:32:49 +0100

    frontend for board implemented. not logic though for it to work

--- minesweeper/config.py
+++ minesweeper/config.py
@@ -3,21 +3,21 @@ difficulty_mapping = {
         "name": "beginner",
         "width": 9,
         "height": 9,
-        "cell_width": "25px",
+        "cell_width": "24px",
         "mine_count": 10
     },
     "intermediate": {
         "name": "intermediate",
         "width": 16,
         "height": 16,
-        "cell_width": "25px",
+        "cell_width": "24px",
         "mine_count": 40
     },
     "expert": {
         "name": "expert",
         "width": 30,
         "height": 16,
-        "cell_width": "25px",
+        "cell_width": "24px",
         "mine_count": 99
     }
 }
