015c2ee179ae38b588aa33a09e6d12948fc4cce3
remles <115708921+remles1@users.noreply.github.com>
2025-02-25 19:28:08 +0100

    added profile

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from accounts.models import ProfileModel
+
 
 class Index(TemplateView):
     template_name = 'index.html'
@@ -9,3 +11,13 @@ class Index(TemplateView):
 
 class AccountProfile(TemplateView):
     template_name = 'account_profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        player = self.request.user
+
+        profile = ProfileModel.objects.get(player=player)
+
+        context['profile'] = profile
+        return context
